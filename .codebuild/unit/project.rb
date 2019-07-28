a052336d068edb84986832d8f3db1ddbf68aff4c
Tung Nguyen <tongueroo@gmail.com>
2019-07-28 18:34:28 +0000

    separate unit and deploy build projects

---
+++ .codebuild/unit/project.rb
@@ -0,0 +1,5 @@
+github_url("https://github.com/tongueroo/jets-codebuild")
+linux_image("timbru31/ruby-node:2.5") # currently must used ruby 2.5 for Lambda
+environment_variables(
+  JETS_ENV: Codebuild.env,
+)
