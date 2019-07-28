a6244baa11925b6e4d461f17249fe621e939eb17
Tung Nguyen <tongueroo@gmail.com>
2019-07-28 18:18:27 +0000

    example with aws/codebuild/ruby image

--- .codebuild/project.rb
+++ .codebuild/project.rb
@@ -1,5 +1,5 @@
-github_url("https://github.com/tongueroo/jets-codebuild")
-linux_image("timbru31/ruby-node:2.5") # currently must used ruby 2.5 for Lambda
+github_url("https://github.com/tongueroo/demo-test")
+linux_image("aws/codebuild/ruby:2.5.3-1.7.0") # currently must used ruby 2.5
 environment_variables(
   JETS_ENV: Codebuild.env,
 )
